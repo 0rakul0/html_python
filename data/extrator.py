b15d39bb51d4275e66834959a0cb56598a9a3f92
Jefferson dos Anjos <0rakul0render@gmail.com>
2022-06-02 19:35:58 -0300

    arquivos externos
    base de arquivo externo

---
+++ data/extrator.py
@@ -0,0 +1,15 @@
+class Extrator():
+    def __init__(self, nome):
+        self.nome = nome
+    
+    def get_name(self):
+        return self.nome
+
+###############################################################################
+"""o que ficaria em 
+if __name__ == '__main__':
+    extrator = Extrator('João')
+    print(extrator.get_name())
+
+fica no py-script dentro do html
+"""
